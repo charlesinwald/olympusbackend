68a4c91e38a82d4041a1f8260947e1f9175e4713
cci219 <cci219@lehigh.edu>
2018-03-02 19:56:16 -0500

    Android+Backend can successfully recognize text

--- base/index.js
+++ base/index.js
@@ -73,76 +73,41 @@ app.post('/upload', function(req, res) {
     return res.status(400).send('No files were uploaded.');
 
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  let sampleFile = req.files.sampleFile;
+   //let sampleFile = req.files.sampleFile;
+  let sampleFile = req.files.file.data;
+ 
   var x = req.body;
-  console.log("body " + x);
+  //console.log("body " + x);
+//   console.log("req" + JSON.stringify(req));
+//   console.log("req" + req.toString());
+//   console.dir(req,1);
+  //console.log("body contents: " + JSON.stringify(x));
+//   console.log("files" + req.files.toString());
+  //console.log("file" + req.files.file.data);
   console.log(sampleFile);
+  console.dir(sampleFile);
   var mv = require('mv');
   var fs = require('fs');
-
+  //console.log(typeof sampleFile);
+  //console.dir(sampleFile);
   //Use the mv() method to place the file somewhere on your server
-  sampleFile.mv('/home/cabox/workspace/base/d.jpg', function(err) {
-    if (err) {
-      return res.status(500).send("Line 108 " + err);
+  fs.writeFile("/home/cabox/workspace/base/public/image/d.jpg", sampleFile, function(err) {
+    if(err) {
+        return console.log(err);
     }
-  });
-  var imageAsBase64 = fs.readFileSync('/home/cabox/workspace/base/d.jpg');
+
+    console.log("The file was saved!");
+}); 
+//   sampleFile.mv('/home/cabox/workspace/base/public/image/d.jpg', function(err) {
+//     if (err) {
+//       return res.status(500).send("Line 108 " + err);
+//     }
+//   });
+  var imageAsBase64 = fs.readFileSync('/home/cabox/workspace/base/public/image/d.jpg');
   var encoded = new Buffer(imageAsBase64).toString('base64');
   console.log("ENCODED: " + encoded);
   console.log(typeof sampleFile);
-  //   var request = require("request");
-  var filename = '/home/cabox/workspace/base/d.jpg';
-  //   var options = {
-  //     method: 'POST',
-  //     url: 'https://vision.googleapis.com/v1/images:annotate',
-  //     qs: {
-  //       key: 'AIzaSyAWgZFieZJBX0ImbMmOuT9GKeLiiPJnfmo'
-  //     },
-  //     headers: {
-  //       'Postman-Token': 'b294a5c1-0cae-4ec9-bca4-d94e5f07a17e',
-  //       'Cache-Control': 'no-cache',
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: {
-  //       requests: [{
-  //         image: {
-  //           //           source: {
-  //           //              gcsImageUri: 'gs://cinwald-bucket/IMG_0178.jpg'
-  //           //           }
-  //           source: {
-  //             imageUri: "http://olympus-cci219706483.codeanyapp.com:1337/image/"
-  //           }
-  //           //           content: "x"
-  //         },
-  //         features: [{
-  //           type: 'TEXT_DETECTION',
-  //           maxResults: 10
-  //         }]
-  //       }]
-  //     },
-  //     json: true
-  //   };
-  //   options.body.requests[0].image.content = fs.readFileSync(filename).toString('base64');
-
-  //   console.log("CONTENT:" + options.body.requests[0].image.content);
-  //   console.log("BASE64" + sampleFile.toString('base64'));
-  //   var JSONrequests = options.body.requests;
-  // //   var JSONimage = JSONrequests[0].image.source.gcsImageUri;
-  //   console.log("JSONrequests " + JSONrequests);
-  //   JSONimage = "x";
-  //   console.log("JSONimage " +JSONimage);
-
-  //   console.log(JSONrequests[0].image.source.gcsImageUri);
-
-
-  //   request(options, function(error, response, body) {
-  //     if (error) throw new Error(error);
-  //     //console.log(body);
-  //     var text = body.responses[0].textAnnotations[0].description;
-  //     console.log(text);
-  //     //res.send(text);
-  //     res.send(body);
-  //   });
+  var filename = '/home/cabox/workspace/base/public/image/d.jpg';
 
   var request = require("request");
 
@@ -161,18 +126,22 @@ app.post('/upload', function(req, res) {
 
   request(options, function(error, response, body) {
     if (error) throw new Error(error);
+    var body2 = JSON.parse(body);
     console.log(body);
-//     var text = body.responses[0].textAnnotations[0].description;
-
-//     console.log(text);
-    res.send(body);
+    var text = body2.responses[0].textAnnotations[0].description;
+    console.log(text);
+    var parsed = parseModel(text);
+    console.log(parsed[0]);
+    res.send(text);
   });
 
 });
 
 //Parse image recognition output into model & serial.
-function parse(imgTxt){
-  var parsedText = imgTxt.match(/\S{3}-\S{4}\n\d{7}/).split("");
+function parseModel(imgTxt) {
+  var parsedText = imgTxt.match(/\S{3}-\S{4}\n\d{7}/);
+  //console.log("PARSED TEXT: " + parsedText);
+  return parsedText;
 }
 // setup server
 var server = app.listen(1337);
