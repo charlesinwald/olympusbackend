b0029a7d3bfc2d03a051ebaa7e22e6089635f30a
cci219 <cci219@lehigh.edu>
2018-03-07 09:09:55 -0500

    cleaner code and comments

--- base/index.js
+++ base/index.js
@@ -9,6 +9,7 @@ var upload = multer({
   dest: 'uploads/'
 })
 
+//used for parsing JSON body
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({
   extended: true
@@ -31,13 +32,14 @@ app.use(less(path.join(__dirname, 'source', 'less'), {
   },
   force: true,
 }));
+
 // Returns the current date and time 
 function myDateTime() {
   return Date()
 }
 
 
-//Prints the current path
+//Prints the current path, helper function
 function currentPath(req) {
   console.log(req.originalUrl); // '/admin/new'
   console.log(req.baseUrl); // '/admin'
@@ -47,24 +49,14 @@ function currentPath(req) {
 // serve static content
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Sets up the admin route
-app.use('/admin', function(req, res, next) { // GET 'http://www.example.com/admin/new'
-  currentPath(req);
-  res.send(myDateTime());
-  next();
-});
+
+//hosts the current image
 app.use('/image', function(req, res) {
   var fs = require('fs');
   var image = fs.readFileSync('/home/cabox/workspace/base/d.jpg');
   res.send(image);
 });
-app.post('/database', function(req, res) {
-  res.send('POST request to homepage');
-  var x = req.body;
-  console.log(x);
-  console.log(x.x);
-  console.log("RES:" + res.responseText);
-});
+
 
 
 app.use(fileUpload());
@@ -72,37 +64,23 @@ app.post('/upload', function(req, res) {
   if (!req.files)
     return res.status(400).send('No files were uploaded.');
 
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-   //let sampleFile = req.files.sampleFile;
+  // The name of the input field used to retrieve the uploaded file
   let sampleFile = req.files.file.data;
- 
+
   var x = req.body;
-  //console.log("body " + x);
-//   console.log("req" + JSON.stringify(req));
-//   console.log("req" + req.toString());
-//   console.dir(req,1);
-  //console.log("body contents: " + JSON.stringify(x));
-//   console.log("files" + req.files.toString());
-  //console.log("file" + req.files.file.data);
   console.log(sampleFile);
   console.dir(sampleFile);
   var mv = require('mv');
   var fs = require('fs');
-  //console.log(typeof sampleFile);
-  //console.dir(sampleFile);
-  //Use the mv() method to place the file somewhere on your server
+
+  //Use the mv() method to place the file somewhere on server
   fs.writeFile("/home/cabox/workspace/base/public/image/d.jpg", sampleFile, function(err) {
-    if(err) {
-        return console.log(err);
+    if (err) {
+      return console.log(err);
     }
 
     console.log("The file was saved!");
-}); 
-//   sampleFile.mv('/home/cabox/workspace/base/public/image/d.jpg', function(err) {
-//     if (err) {
-//       return res.status(500).send("Line 108 " + err);
-//     }
-//   });
+  });
   var imageAsBase64 = fs.readFileSync('/home/cabox/workspace/base/public/image/d.jpg');
   var encoded = new Buffer(imageAsBase64).toString('base64');
   console.log("ENCODED: " + encoded);
@@ -123,7 +101,7 @@ app.post('/upload', function(req, res) {
     },
     body: '{\n  "requests":[\n    {\n      "image":{\n        "source":{\n          "imageUri":\n            "http://olympus-cci219706483.codeanyapp.com:1337/image/d.jpg"\n        }\n      },\n      "features":[\n        {\n          "type":"TEXT_DETECTION",\n          "maxResults":1\n        }\n      ]\n    }\n  ]\n}'
   };
-
+  //Google Cloud Vision Request
   request(options, function(error, response, body) {
     if (error) throw new Error(error);
     var body2 = JSON.parse(body);
@@ -132,15 +110,15 @@ app.post('/upload', function(req, res) {
     console.log(text);
     var parsed = parseModel(text);
     console.log(parsed[0]);
-    res.send(text);
+    res.send(parsed[0]);
   });
 
 });
 
+
 //Parse image recognition output into model & serial.
 function parseModel(imgTxt) {
   var parsedText = imgTxt.match(/\S{3}-\S{4}\n\d{7}/);
-  //console.log("PARSED TEXT: " + parsedText);
   return parsedText;
 }
 // setup server
