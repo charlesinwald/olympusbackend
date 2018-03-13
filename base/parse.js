c91900b1f33dbc665914764290dff67f452a4b08
cci219 <cci219@lehigh.edu>
2018-03-13 10:35:47 -0400

    Generated a tsconfig file

--- base/parse.js
+++ base/parse.js
@@ -3,7 +3,7 @@ exports.__esModule = true;
 //module.exports = {
 //Parse image recognition output into model & serial.
 function parseModel(imgTxt) {
-    return imgTxt.match(/\S{3}-\S{5}\n\d{7}/);
+    return imgTxt.match(/\S{3}-\S{5}\n\d{7}/ || /\S{3}-\S{4}\n\d{7}/);
 }
 exports.parseModel = parseModel;
 //};
--- base/parse.ts
+++ base/parse.ts
@@ -1,6 +1,6 @@
 //module.exports = {
  //Parse image recognition output into model & serial.
 export function parseModel(imgTxt){
-    return imgTxt.match(/\S{3}-\S{5}\n\d{7}/);
+    return imgTxt.match(/\S{3}-\S{5}\n\d{7}/||/\S{3}-\S{4}\n\d{7}/);
 }
 //};
